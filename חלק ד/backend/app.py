676ffe97b15a2c79c9cd5673912969fa8baf551a
Chani Kahana <c0527107796@gmail.com>
2025-04-09 20:07:22 +0300

    refactor

--- חלק ד/backend/app.py
+++ חלק ד/backend/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS
+from flask_cors import CORS # הוספת ספריה
 
 load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
